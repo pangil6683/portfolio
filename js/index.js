90b2158212ffefbc2ef40b6bd67050db603c4ffa
pangil6683 <oksk6684@naver.com>
2021-04-13 04:25:50 +0900

    레이아웃 설정2

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@ const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  icons.classList.toggle('active');
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
 });
+
+var pageName = location.href.split("/");
+pageName = pageName[4].split(".");
+// alert(pageName[0]);
+
+var test = document.getElementById(pageName[0]);
+test.style.backgroundColor = "#3078c0";
+test.style.borderRadius = "4px";
